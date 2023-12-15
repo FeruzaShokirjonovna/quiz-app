ab7099d4dbf0aa675d4c8573f25a452bd6323fe7
Feruza Orifjonova <feruza.orifjonova1997@gmail.com>
2023-12-15 00:58:12 +0000

    Add quiz section with some style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
 const rulesBtn = document.querySelector('.rules-btn');
 const rulesInfo = document.querySelector('.rules-info');
+const exitBtn = document.querySelector('.info-btn-exit');
+const main = document.querySelector('.main');
 
+/***
+ * Rules info activates when the rules button clicked
+ */
 rulesBtn.onclick = () => {
     rulesInfo.classList.add('active');
+    main.classList.add('active');
+}
+/***
+ * Exit button removes Rules info
+ */
+exitBtn.onclick = () => {
+    rulesInfo.classList.remove('active');
+    main.classList.remove('active');
 }
