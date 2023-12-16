7ff39d4192304eeb539b1acf548d0233aea51ab9
Feruza Orifjonova <feruza.orifjonova1997@gmail.com>
2023-12-16 11:23:32 +0000

    Add function to randomize questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,10 @@ const quizSection = document.querySelector('.quiz-section');
 const startBtn = document.querySelector('.start-btn')
 const quizBox = document.querySelector('.quiz-box')
 
+const questionText = document.querySelector('.question-text');
+const questionNumber = document.querySelector('.question-number')
+const optionsList = document.querySelector('.options-list')
+
 /***
  * Rules info activates when the rules button clicked
  */
@@ -34,16 +38,55 @@ startBtn.onclick = () => {
     rulesInfo.classList.remove('active');
     main.classList.remove('active');
     quizBox.classList.add('active');
-
-    showQuestions
 }
 
-const nextBtn = document.querySelector('.next')
+let questionCount = 0;
+let currentQuestion;
+let availableQuestions = [];
+
+const nextBtn = document.querySelector('.next-btn')
+
+/***
+ * Function to push questions into availableQuestions
+ */
+function setAvailableQuestions() {
+    const totalQuestion = questions.length;
+    for(let i=0; i<totalQuestion; i++){
+        availableQuestions.push(questions[i]);
+    }
+}
+
+/***
+ * Function to get new question and options
+ */
+function getNewQuestion(){
+    //set question number
+    questionNumber.innerHTML = "Question" + (questionCount +1) + "of 10";
+    //set question text
+    //randomize questions
+    const questionIndex = availableQuestions[Math.floor(Math.random() * availableQuestions.length)];
+    currentQuestion = questionIndex;
+    questionText.innerHTML = currentQuestion.question;
+    
+    questionCount++
+}
+
+function next(){
+    if(questionCount === questions.length){
+        console.log("quiz over")
+    }
+    else{
+        getNewQuestion();
+    } 
+}
+
+window.onload = function(){
+    //Set all questions in availableQuestions Array first
+    setAvailableQuestions();
+    //Second set new question among available questions array
+    getNewQuestion();
+}
 
 /***
  * Function to get questions from the array
  */
-function showQuestions() {
-    const questionText = document.querySelector('.question-text');
-    questionText.textContent = '${questions[index].}';
-}
