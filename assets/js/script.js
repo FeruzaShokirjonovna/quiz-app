320901a83fdb8a958ae85013c17a3c9ea5fab3f8
Feruza Orifjonova <feruza.orifjonova1997@gmail.com>
2023-12-19 20:54:57 +0000

    Add missing button references

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     const nameInput = document.getElementById('name-input');
     nameInput.focus();
+    
+    // Add the missing button references
+    const rulesBtn = document.querySelector('.rules-btn');
+    const exitBtn = document.querySelector('.info-btn-exit');
+    const tryAgainBtn = document.querySelector('.tryagain-btn');
+    const startBtn = document.querySelector('.start-btn');
 
+    rulesBtn.addEventListener('click', clickedButtonHandler);
+    exitBtn.addEventListener('click',clickedButtonHandler);
     tryAgainBtn.addEventListener('click', clickedButtonHandler);
+    startBtn.addEventListener('click', clickedButtonHandler);
 
     function clickedButtonHandler(event) {
         const clickedButton = event.target;
@@ -11,9 +20,27 @@ document.addEventListener('DOMContentLoaded', function () {
         if (clickedButton === tryAgainBtn) {
             resetQuiz();
         }
+        // Quiz starts when start button clicked
+        if (clickedButton === startBtn) {
+            quizSection.classList.add('active');
+            rulesInfo.classList.remove('active');
+            main.classList.remove('active');
+            quizBox.classList.add('active');
+            startQuiz(username);
+        }
+        // Rules info activates when the rules button clicked
+        if (clickedButton === rulesBtn) {
+            rulesInfo.classList.add('active');
+            main.classList.add('active');
+        }
+        // Exit button removes Rules info
+        if (clickedButton === exitBtn) {
+            rulesInfo.classList.remove('active');
+            main.classList.remove('active');
+        }
     }
 });
- 
+
 // Global variables to access from more than 1 functions
 const rulesBtn = document.querySelector('.rules-btn');
 const rulesInfo = document.querySelector('.rules-info');
@@ -38,25 +65,6 @@ const scoreMessageRef = document.querySelector("#score-message");
  * Event listeners to access all buttons
  */
 
-// Rules info activates when the rules button clicked
-rulesBtn.onclick = () => {
-    rulesInfo.classList.add('active');
-    main.classList.add('active');
-}
-
-// Exit button removes Rules info
-exitBtn.onclick = () => {
-    rulesInfo.classList.remove('active');
-    main.classList.remove('active');
-}
-
-startBtn.onclick = () => {
-    quizSection.classList.add('active');
-    rulesInfo.classList.remove('active');
-    main.classList.remove('active');
-    quizBox.classList.add('active');
-    startQuiz(username);
-}
 
 let questionCount = 0;
 let currentQuestion;
@@ -65,8 +73,9 @@ let availableOptions = [];
 let correctAnswers = 0;
 let attempt = 0;
 
-const nextBtn = document.querySelector('.next-btn')
+const nextBtn = document.querySelector('.next-btn');
 const username = document.querySelector("#name-input").value;
+
 /**
  * Checking if the input is empty, to throw an alert
  */
@@ -84,7 +93,7 @@ const checkUsername = (username) => {
  */
 function setAvailableQuestions() {
     const totalQuestion = questions.length;
-    for(let i=0; i<totalQuestion; i++){
+    for (let i = 0; i < totalQuestion; i++) {
         availableQuestions.push(questions[i]);
     }
 }
@@ -92,9 +101,9 @@ function setAvailableQuestions() {
 /***
  * Function to get new question and options
  */
-function getNewQuestion(){
+function getNewQuestion() {
     //set question number
-    questionNumber.innerHTML = "Question" + " "+ (questionCount + 1) + " " + "of 5";
+    questionNumber.innerHTML = "Question" + " " + (questionCount + 1) + " " + "of 5";
     //set question text
     //randomize questions
     const questionIndex = availableQuestions[Math.floor(Math.random() * availableQuestions.length)];
@@ -103,49 +112,49 @@ function getNewQuestion(){
     //get the position of "questionIndex" from the questions array
     const index1 = availableQuestions.indexOf(questionIndex);
     //remove the question index, that is why once given question will not repeated
-    availableQuestions.splice(index1,1);
-    
+    availableQuestions.splice(index1, 1);
+
     //set options
     //get the length of options
     const optionLen = currentQuestion.options.length
     //push options to available options array
-    for(let i=0; i<optionLen; i++){
+    for (let i = 0; i < optionLen; i++) {
         availableOptions.push(i)
     }
     //remove old options
     optionsList.innerHTML = '';
 
     //create options in html
-    for(let i=0; i<optionLen; i++){
+    for (let i = 0; i < optionLen; i++) {
         //random option
         const optionIndex = availableOptions[Math.floor(Math.random() * availableOptions.length)];
-        
+
         // get the position of optionIndex from available options
         const index2 = availableOptions.indexOf(optionIndex);
-       
+
         //remove the option from options array, then option will not repeat
-        availableOptions.splice(index2,1);
+        availableOptions.splice(index2, 1);
         const option = document.createElement("div");
         option.innerHTML = currentQuestion.options[optionIndex];
         option.id = optionIndex;
-        
+
         option.className = "option";
         optionsList.appendChild(option)
         option.setAttribute('onclick', 'getResult(this)');
     }
 
     questionCount++
-    
+
 }
 
 /***
  * Function to get the result of current attempt
  */
-function getResult(optionElement){
+function getResult(optionElement) {
     const id = parseInt(optionElement.id);
 
     //get the answer by comparing the id of the clicked option
-    if(id === currentQuestion.answer){
+    if (id === currentQuestion.answer) {
         //set green background color if it is true
         optionElement.classList.add('correct');
         //add mark to correct answer
@@ -153,15 +162,15 @@ function getResult(optionElement){
         correctAnswers++;
         console.log("correct:" + correctAnswers)
     }
-    else{
+    else {
         //set red background color if it is wrong
         optionElement.classList.add('wrong');
         //add mark to incorrect answer
         updateScoreIndicator('wrong');
         //show the true answer in green if user's answer is wrong
         const optionLen = optionsList.children.length;
-        for(let i=0; i<optionLen; i++){
-            if(parseInt(optionsList.children[i].id) === currentQuestion.answer){
+        for (let i = 0; i < optionLen; i++) {
+            if (parseInt(optionsList.children[i].id) === currentQuestion.answer) {
                 optionsList.children[i].classList.add('correct');
             }
         }
@@ -173,9 +182,9 @@ function getResult(optionElement){
 /***
  * Function to choose one option per question
  */
-function unclickableOptions(){
+function unclickableOptions() {
     const optionLen = optionsList.children.length;
-    for(let i=0; i<optionLen; i++){
+    for (let i = 0; i < optionLen; i++) {
         optionsList.children[i].classList.add('answered-once');
     }
 }
@@ -186,7 +195,7 @@ function unclickableOptions(){
 function scoreIndicator() {
     scoreIndicatorContainer.innerHTML = '';
     const totalQuestion = 5;
-    for(let i=0; i<totalQuestion; i++){
+    for (let i = 0; i < totalQuestion; i++) {
         const indicator = document.createElement('div');
         scoreIndicatorContainer.appendChild(indicator);
     }
@@ -195,25 +204,29 @@ function scoreIndicator() {
 /***
  * Function to update score indicator
  */
-function updateScoreIndicator(markType){
-    scoreIndicatorContainer.children[questionCount-1].classList.add(markType)
+function updateScoreIndicator(markType) {
+    scoreIndicatorContainer.children[questionCount - 1].classList.add(markType)
 
 }
 
-function next(){
-    if(questionCount === 5){
+/***
+ * Function to change the questions when clicked next button,
+ * if questions are over, finishes quiz 
+ */
+function next() {
+    if (questionCount === 5) {
         console.log("quiz over");
         quizOver();
     }
-    else{
+    else {
         getNewQuestion();
-    } 
+    }
 }
 
 /***
  * Function to finish the quiz
  */
-function quizOver(){
+function quizOver() {
     homeBox.classList.add('active');
     quizSection.classList.remove('active');
     resultBox.classList.add('active');
@@ -225,32 +238,32 @@ function quizOver(){
 /***
  * Function to get the user result 
  */
-function quizResult(){
+function quizResult() {
     const totalQuestion = 5;
 
-    resultBox.querySelector('.total-question').innerHTML= totalQuestion;
-    resultBox.querySelector('.total-attempt').innerHTML=attempt;
-    resultBox.querySelector('.total-correct').innerHTML=correctAnswers;
-    resultBox.querySelector('.total-wrong').innerHTML=attempt-correctAnswers;
+    resultBox.querySelector('.total-question').innerHTML = totalQuestion;
+    resultBox.querySelector('.total-attempt').innerHTML = attempt;
+    resultBox.querySelector('.total-correct').innerHTML = correctAnswers;
+    resultBox.querySelector('.total-wrong').innerHTML = attempt - correctAnswers;
     const percentage = (correctAnswers / totalQuestion) * 100;
-    resultBox.querySelector('.percentage').innerHTML= percentage.toFixed(2) + "%";
-    resultBox.querySelector('.total-score').innerHTML= correctAnswers + " / " + 5;
+    resultBox.querySelector('.percentage').innerHTML = percentage.toFixed(2) + "%";
+    resultBox.querySelector('.total-score').innerHTML = correctAnswers + " / " + 5;
 }
 
 /***
  * Function to restart the quiz
  */
-function resetQuiz(){
-    correctAnswers=0;
-    attempt=0;
-    questionCount=0;
+function resetQuiz() {
+    correctAnswers = 0;
+    attempt = 0;
+    questionCount = 0;
     startQuiz(username);
 }
 
 /***
  * Function to start the game
  */
-function startQuiz(username){
+function startQuiz(username) {
     // Check if the username is valid
     checkUsername();
     //Set all questions in availableQuestions Array first
